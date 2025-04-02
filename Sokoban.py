54f25f2dd5ab2d270e64527b4cd0b7c25a072248
Rosas Behoundja <perrierosas@gmail.com>
2025-04-03 00:24:19 +0100

    Refactor Sokoban game logic and add solver module; update .gitignore to exclude cache files

--- Sokoban.py
+++ Sokoban.py
@@ -8,7 +8,6 @@ class Sokoban(Problem):
         self.initial = initial
         self.targets = targets
         self.walls = walls
-        self.sequences = []
 
     def actions(self, state):
         """
@@ -31,7 +30,7 @@ class Sokoban(Problem):
                 new_box_x = new_x + dx
                 new_box_y = new_y + dy
 
-                if (new_box_x, new_box_y) not in self.walls and (new_box_x, new_box_y) not in boxes:
+                if (new_box_x, new_box_y) not in self.walls and (new_box_x, new_box_y) not in boxes and not self.is_dead_state((new_box_x, new_box_y)):
                     valid_actions.append(action)
             else:
                 valid_actions.append(action)
@@ -61,18 +60,17 @@ class Sokoban(Problem):
 
         return (new_agent, frozenset(new_boxes))
     
-    def is_dead_state(self, boxes):
-        """Vérifie si les boites se retrouvent dans des angles morts"""
-        for box in boxes:
-            if box not in self.targets:
-                x, y = box
-                # Vérifier si la boîte est dans un coin
-                left_wall = (x-1, y) in self.walls
-                right_wall = (x+1, y) in self.walls
-                up_wall = (x, y-1) in self.walls
-                down_wall = (x, y+1) in self.walls
-                if (left_wall or right_wall) and (up_wall or down_wall):
-                    return True
+    def is_dead_state(self, box):
+        """Vérifie si la boite se retrouve dans des angles morts"""
+        if box not in self.targets:
+            x, y = box
+            # Vérifier si la boîte est dans un coin
+            left_wall = (x-1, y) in self.walls
+            right_wall = (x+1, y) in self.walls
+            up_wall = (x, y-1) in self.walls
+            down_wall = (x, y+1) in self.walls
+            if (left_wall or right_wall) and (up_wall or down_wall):
+                return True
         return False
 
     def goal_test(self, state):
@@ -82,35 +80,25 @@ class Sokoban(Problem):
         _, boxes = state
         return all(box in self.targets for box in boxes)
     
-    def heuristic(self, node):
+    def h(self, node):
         """Fonction heuristique pour les algorithmes de recherche informée"""
         total = 0
-        for box in node.state[1]:
+        agent, boxes = node.state
+        for box in boxes:
             if box not in self.targets:
                 min_dist = min(abs(box[0] - t[0]) + abs(box[1] - t[1]) for t in self.targets)
                 total += min_dist
+                min_agent_dist = min(abs(agent[0] - t[0]) + abs(agent[1] - t[1]) for t in self.targets)
+                total += min_agent_dist*0.5 # Reduire l'influence de la position de l'agent
         return total
     
     def path_cost(self, c, state1, action, state2):
         """
         Return cost of path from state1 to state2 given action
         """
+        _, boxes1 = state1
+        _, boxes2 = state2
+
+        if boxes1 != boxes2:
+            return c + 2
         return c + 1
-    
-
-if __name__ == "__main__":
-    from aima.search import astar_search, greedy_best_first_graph_search
-
-    initial_agent, initial_boxes, walls, width, height, targets = getGameBoard("levels/sokoInst02.init", "levels/sokoInst02.goal")
-    # Initialisation
-    problem = Sokoban(initial=(initial_agent, initial_boxes), targets=targets, walls=walls)
-
-    # Résolution avec A*
-    print(">> A*")
-    solution = astar_search(problem, h= problem.heuristic, display= True)
-    # print(">> GBS")
-    # solution = greedy_best_first_graph_search(problem, f = problem.heuristic, display=True)
-    if solution:
-        for state in solution.path():
-            print_state(state.state, walls, width=width, height=height, targets=targets)
-            print()
